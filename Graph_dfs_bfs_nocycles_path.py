28aa20158bc9fb7bd2a32cd61e2134c8a1dc15e7
Mihiru Kongahage <mihirukongahage@gmail.com>
2018-11-06 13:15:11 +0530

    DFS,BFS,PATH

---
+++ Graph_dfs_bfs_nocycles_path.py
@@ -0,0 +1,79 @@
+''' Directed Graph '''
+''' No Weights '''
+''' Oper : BFS, DFS, No of cycles, path '''
+
+
+graph = {}
+
+def add_node(src, dest):
+    if(src not in graph):
+        graph[src] = []
+    if(dest not in graph):
+        graph[dest] = []
+     
+    graph[src].append(dest)
+
+
+def bfs(start):
+    visited = []
+    q = [start]
+    while(q):
+        p = q.pop(0)
+        if(p not in visited):
+            visited.append(p)
+            for v in graph[p]:
+                q.append(v)
+    
+    return visited
+
+def dfs(start):
+    visited = []
+    s = [start]
+    while(s):
+        p = s.pop(-1)
+        if(p not in visited):
+            visited.append(p)
+            for v in graph[p]:
+                s.append(v)
+    
+    return visited
+
+def no_of_cyc(start):
+    count  = 0
+    visited = []
+    s = [start]
+    while(s):
+        p = s.pop(-1)
+        if(p not in visited):
+            visited.append(p)
+            for v in graph[p]:
+                s.append(v)
+        else:
+            count = count + 1
+    return count
+
+def f_path(start, end, path = []):
+    path = path + [start]
+    if(start == end):
+        return path
+    for node in graph[start]:
+        if node not in path:
+            new_path = f_path(node, end, path)
+            if new_path:
+                return new_path
+    return None
+            
+    
+    
+
+if __name__ == "__main__":
+    add_node(0,1)
+    add_node(1,2)
+    add_node(0,2)
+    add_node(2,0)
+    add_node(2,3)
+    add_node(3,3)
+
+
+    
+    print(f_path(0,3))
